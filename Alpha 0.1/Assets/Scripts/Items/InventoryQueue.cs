5f9ed839c1903f1a4b651441aa0aeac7cbd870ab
Tironosauroo <urcenkot491@gmail.com>
2025-10-01 01:28:54 +0300

    Inventory Queue rework

---
+++ Alpha 0.1/Assets/Scripts/Items/InventoryQueue.cs
@@ -0,0 +1,69 @@
+using System;
+
+public class InventoryQueue<T> : IInventoryQueue<T>
+{
+    private T[] _array;
+    private int _head;
+    private int _tail;
+    private int _size;
+    private int _capacity;
+
+    public InventoryQueue(int capacity = 4)
+    {
+        _capacity = capacity;
+        _array = new T[_capacity];
+        _head = 0;
+        _tail = 0;
+        _size = 0;
+    }
+
+    public int Count => _size;
+
+    public void Enqueue(T item)
+    {
+        if (_size == _capacity)
+            Resize();
+
+        _array[_tail] = item;
+        _tail = (_tail + 1) % _capacity;
+        _size++;
+    }
+
+    public T Dequeue()
+    {
+        if (_size == 0)
+            throw new InvalidOperationException("Queue is empty");
+
+        T item = _array[_head];
+        _array[_head] = default(T);
+        _head = (_head + 1) % _capacity;
+        _size--;
+        return item;
+    }
+
+    public T[] IQtoArray()
+    {
+        T[] result = new T[_size];
+        for (int i = 0; i < _size; i++)
+        {
+            result[i] = _array[(_head + i) % _capacity];
+        }
+        return result;
+    }
+
+    private void Resize()
+    {
+        int newCapacity = _capacity * 2;
+        T[] newArray = new T[newCapacity];
+
+        for (int i = 0; i < _size; i++)
+        {
+            newArray[i] = _array[(_head + i) % _capacity];
+        }
+
+        _array = newArray;
+        _head = 0;
+        _tail = _size;
+        _capacity = newCapacity;
+    }
+}
